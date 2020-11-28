efccc4a5fe989cc998da96ccd65f30f76c09dbb9
JC Lounsbery <knightigris7@gmail.com>
2020-11-28 18:46:41 -0500

    Revert files

--- Assets/Scripts/CustomEnemyAI.cs
+++ Assets/Scripts/CustomEnemyAI.cs
@@ -5,20 +5,12 @@ using Pathfinding;
 
 public class CustomEnemyAI : MonoBehaviour
 {
-<<<<<<< HEAD
-        
-    public Transform target;
-    public float speed = 200f;
-    public float nextWaypointDistance = 3f;
-    public Rigidbody2D rb;
-=======
     public Transform target;
     public float speed = 200f;
     public float nextWaypointDistance = 3f;
     private Rigidbody2D rb;
 
     private Vector3 currentMove = new Vector3();
->>>>>>> d873e4d20001b41d3c806a382522c2d0e34ef9d3
 
     Path path;
     int currentWaypoint =0;
@@ -31,18 +23,6 @@ public class CustomEnemyAI : MonoBehaviour
     {
         seeker = GetComponent<Seeker>();
         rb= GetComponent<Rigidbody2D>();    
-<<<<<<< HEAD
-
-
-        //this is key with timing with player
-        InvokeRepeating("UpdatePath", 0f,.5f);
-
-    }
-
-    void UpdatePath(){
-        seeker.StartPath(rb.position, target.position, OnPathComplete);
-    }
-=======
     }
     void Update(){
     if (Input.GetKeyDown(KeyCode.W))
@@ -50,7 +30,6 @@ public class CustomEnemyAI : MonoBehaviour
             moveEnemy();
         }
 }
->>>>>>> d873e4d20001b41d3c806a382522c2d0e34ef9d3
 
     void OnPathComplete(Path p){
         if (!p.error){
@@ -62,16 +41,10 @@ public class CustomEnemyAI : MonoBehaviour
     
 
     // Update is called once per frame
-<<<<<<< HEAD
-    public void enemyMove()
-    {
-        //Vector3 movePosition = new Vector3(0, 0);
-=======
     void moveEnemy()
     {
         seeker.StartPath(rb.position, target.position, OnPathComplete);
 
->>>>>>> d873e4d20001b41d3c806a382522c2d0e34ef9d3
         if (path ==null)
             return;
 
@@ -82,25 +55,6 @@ public class CustomEnemyAI : MonoBehaviour
         else{
             reachedEndOfPath = false;
         }
-<<<<<<< HEAD
-
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] -rb.position),normalized;
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-        if(distance < nextWaypointDistance){
-            currentWaypoint++;
-        }
-        Debug.Log(direction);
-        // if(Input.GetKeyDown(KeyCode.W)) {
-        //     movePosition = new Vector3(transform.position.x + 1, transform.position.y);
-        //     transform.position = movePosition;
-        // }
-
-        
-    }
-
-    void Update() {
-        
-=======
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         Debug.Log(direction.y);
         if(direction.y > 0){
@@ -113,6 +67,5 @@ public class CustomEnemyAI : MonoBehaviour
         if(distance < nextWaypointDistance){
             currentWaypoint++;
         }
->>>>>>> d873e4d20001b41d3c806a382522c2d0e34ef9d3
     }
 }
