113a4a8e6138d3ab21cad00f6250adb484c7cdf1
Chris <chris.adonizio@gmail.com>
2020-11-27 21:31:19 -0500

    commit

--- Assets/Scripts/CustomEnemyAI.cs
+++ Assets/Scripts/CustomEnemyAI.cs
@@ -5,67 +5,68 @@ using Pathfinding;
 
 public class CustomEnemyAI : MonoBehaviour
 {
-    public Transform target;
-    public float speed = 200f;
-    public float nextWaypointDistance = 3f;
+        
+//     public Transform target;
+//     public float speed = 200f;
+//     public float nextWaypointDistance = 3f;
+//     public Rigidbody2D rb;
 
-    Path path;
-    int currentWaypoint =0;
-    bool reachedEndOfPath = false;
+//     Path path;
+//     int currentWaypoint =0;
+//     bool reachedEndOfPath = false;
 
-    Seeker seeker;
-    Rigidbody2D rb;
+//     Seeker seeker;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        seeker = GetComponent<Seeker>();
-        rb= GetComponent<Rigidbody2D>();    
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         seeker = GetComponent<Seeker>();
+//         rb= GetComponent<Rigidbody2D>();    
 
 
-        //this is key with timing with player
-        InvokeRepeating("UpdatePath", 0f,.5f);
+//         //this is key with timing with player
+//         InvokeRepeating("UpdatePath", 0f,.5f);
 
-    }
+//     }
 
-    void UpdatePath(){
-        seeker.StartPath(rb.position, target.position, OnPathComplete);
-    }
+//     void UpdatePath(){
+//         seeker.StartPath(rb.position, target.position, OnPathComplete);
+//     }
 
-    void OnPathComplete(Path p){
-        if (!p.error){
-            path = p;
-            currentWaypoint = 0;
-            Vector2 direction = direction = ((Vector2)path.vectorPath[currentWaypoint] -rb.position),normalized;
-            Vector2 force = direction * speed * Time.deltaTime;
-            rb.AddForce(-1*force);
-        }
+//     void OnPathComplete(Path p){
+//         if (!p.error){
+//             path = p;
+//             currentWaypoint = 0;
+//         }
 
-        }
+//         }
     
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (path ==null)
-            return;
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (path ==null)
+//             return;
 
-        if(currentWaypoint >= path.vectorPath.Count){
-            reachedEndOfPath = true;
-            return;
-        }
-        else{
-            reachedEndOfPath = false;
-        }
+//         if(currentWaypoint >= path.vectorPath.Count){
+//             reachedEndOfPath = true;
+//             return;
+//         }
+//         else{
+//             reachedEndOfPath = false;
+//         }
 
-        Vector2 direction = direction = ((Vector2)path.vectorPath[currentWaypoint] -rb.position),normalized;
-        Vector2 force = direction * speed * Time.deltaTime;
+//         Vector2 direction = direction = ((Vector2)path.vectorPath[currentWaypoint] -rb.position),normalized;
+//         Debug.Log(direction);
+//         Vector2 force = direction * speed * Time.deltaTime;
 
-        rb.AddForce(force);
+//         rb.AddForce(force);
 
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-        if(distance < nextWaypointDistance){
-            currentWaypoint++;
-        }
-    }
+//         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+//         if(distance < nextWaypointDistance){
+//             currentWaypoint++;
+//         }
+//     }
+// }
+
 }
