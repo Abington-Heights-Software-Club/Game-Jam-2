24919b2cfdf4c653ac324c3474ac196bce2bb081
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2020-11-28 22:23:54 -0500

    Deer LongIdle Logic Added

--- Assets/Scripts/DeerMovement.cs
+++ Assets/Scripts/DeerMovement.cs
@@ -5,18 +5,17 @@ using Pathfinding;
 
 public class DeerMovement : MonoBehaviour
 {
-    public static float gridSquareSize = 1;
-    public static float moveSpeed = 0.5f;
     public AIDestinationSetter AIDestinationSetter;
 
-    private Vector3 currentMove = new Vector3();
-    //private Vector3 targetPos = new Vector3();
     private float currentDistTraveled; //used to determine how much of each player move is left to make
+    private bool isWaiting = false;
+    private bool isWaitingTimerOn = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
     // Update is called once per frame
     void Update()
@@ -27,21 +26,31 @@ public class DeerMovement : MonoBehaviour
         {
             movedPosition = new Vector3(transform.position.x + 1, transform.position.y);
             moveDirection = Vector2.right;
+            isWaiting = false;
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
             movedPosition = new Vector3(transform.position.x - 1, transform.position.y);
             moveDirection = Vector2.left;
+            isWaiting = false;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             movedPosition = new Vector3(transform.position.x, transform.position.y - 1);
             moveDirection = Vector2.down;
+            isWaiting = false;
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             movedPosition = new Vector3(transform.position.x, transform.position.y + 1);
             moveDirection = Vector2.up;
+            isWaiting = false;
+        }
+        else if(!isWaitingTimerOn)
+        {
+            isWaitingTimerOn = true;
+            isWaiting = true;
+            StartCoroutine(idleWait());
         }
         if(movedPosition != new Vector3(0, 0))
         {
@@ -52,38 +61,20 @@ public class DeerMovement : MonoBehaviour
             }
             AIDestinationSetter.enemyMove();
         }
-
-        /*if (currentMove == new Vector3()) { //currently not moving
-            if (Input.GetAxisRaw("Horizontal") > 0.5f)
-            {
-                currentMove = new Vector3(1, 0);
-            }
-            else if (Input.GetAxisRaw("Horizontal") < -0.5f)
-            {
-                currentMove = new Vector3(-1, 0);
-            }
-            else if (Input.GetAxisRaw("Vertical") > 0.5f)
-            {
-                currentMove = new Vector3(1, 0);
-            }
-            else if (Input.GetAxisRaw("Vertical") < -0.5f)
-            {
-                currentMove = new Vector3(-1, 0);
-            }
-            //targetPos = transform.position + new Vector3(currentMove.x * gridSquareSize, currentMove.y * gridSquareSize);
-            currentDistTraveled = 0f;
-
-        else //currently moving
+    }
+    IEnumerator idleWait()
+    {
+        int count = 0;
+        while(isWaiting)
         {
-            transform.position += new Vector3(currentMove.x * gridSquareSize, currentMove.y * gridSquareSize);
-            
-            if (currentDistTraveled < gridSquareSize)
+            yield return new WaitForSeconds(1);
+            count++;
+            Debug.Log("Time: " + count);
+            if(count == 9)
             {
-                transform.position = transform.position + new Vector3(currentMove.x * gridSquareSize * Time.deltaTime * moveSpeed, currentMove.y * gridSquareSize * Time.deltaTime * moveSpeed);
-                currentDistTraveled = currentDistTraveled + gridSquareSize * Time.deltaTime * moveSpeed;
+                Debug.Log("Waiting Long");
             }
-            if(currentDistTraveled>)
-                
-        }*/
+        }
+        isWaitingTimerOn = false;
     }
 }
