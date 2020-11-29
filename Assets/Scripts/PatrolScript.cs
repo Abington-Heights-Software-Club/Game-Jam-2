f86162594b623733cf880731909676bb27873831
TheSnazzmaster <43121657+TheSnazzmaster@users.noreply.github.com>
2020-11-28 20:18:07 -0800

    patrol script

---
+++ Assets/Scripts/PatrolScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform castPoint;
+    private Vector3 direction = Vector3.right;
+    void Update()
+    {
+        patrol(direction);
+    }
+
+    void patrol(Vector3 direction)
+    {
+        bool val = false;
+        //calculates end position
+        Vector2 endPosition = castPoint.position + direction;
+        //action is player space can include walls 
+        RaycastHit2D hit = Physics2D.Linecast(castPoint.position, endPosition, 1 << LayerMask.NameToLayer("Action"));
+        //if it hits anything
+        if (hit.collider != null)
+        {
+            //if what it hits is wall
+            if (hit.collider.gameObject.layer.Equals("Wall"))
+            {
+                direction = new Vector3(-direction.x, -direction.y);
+            }
+
+        }
+        else //doesnt hit anything
+        {
+            transform.position += direction;
+        }
+        transform.position += direction;
+    }
+}
