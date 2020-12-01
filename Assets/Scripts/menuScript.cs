24fbab3a73bbafa4c04543e4347dffabb126f8af
Fletchlingboy <fletchlingboy080403@gmail.com>
2020-11-30 21:55:27 -0500

    mainMenu and Circle

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuScript : MonoBehaviour
+{
+    public Animator deerAnim;
+    // Start is called before the first frame update
+    public void wakeDeer(){
+        deerAnim.SetBool("wakeDeer",true);
+    }
+}
