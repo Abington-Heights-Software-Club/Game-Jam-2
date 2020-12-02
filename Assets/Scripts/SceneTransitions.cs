f94a8a3f83cd70dad50958ff88c3491c846be47f
Fletchlingboy <fletchlingboy080403@gmail.com>
2020-12-01 21:13:15 -0500

    addedAnimsAndSprites
    main menu scene change, tree sprite, hunter walk

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator sceneAnim;
+    public string sceneName;
+
+    public void changeScene(){
+        StartCoroutine(LoadScene());
+    }
+    IEnumerator LoadScene(){
+        yield return new WaitForSeconds(1.5f);
+        sceneAnim.SetTrigger("endScene");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/Scripts/menuScript.cs
+++ Assets/Scripts/menuScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class menuScript : MonoBehaviour
 {
     public Animator deerAnim;
+    public Rigidbody2D deer;
     // Start is called before the first frame update
     public void wakeDeer(){
         deerAnim.SetBool("wakeDeer",true);
