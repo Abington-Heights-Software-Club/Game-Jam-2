3eceb5d64cfeb20bf2f08bef437998078e3d787c
Fletchlingboy <fletchlingboy080403@gmail.com>
2020-12-10 00:03:34 -0500

    finishedMainMenu
    final main menu probably

--- Assets/Scripts/menuScript.cs
+++ Assets/Scripts/menuScript.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class menuScript : MonoBehaviour
 {
     public Animator deerAnim;
     public GameObject creditsPanel;
     public GameObject mainPanel;
+    public GameObject optionsPanel;
+    public AudioMixer audioMixer;
     // Start is called before the first frame update
     public void wakeDeer(){
         deerAnim.SetBool("wakeDeer",true);
@@ -15,8 +17,16 @@ public class menuScript : MonoBehaviour
         mainPanel.SetActive(false);
         creditsPanel.SetActive(true);
     }
+    public void options(){
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
     public void backToMain(){
         mainPanel.SetActive(true);
         creditsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+    public void SetVolume(float volume){
+        audioMixer.SetFloat("masterVolume",Mathf.Log10(volume)*20);
     }
 }
